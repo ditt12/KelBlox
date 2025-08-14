9a1b91e0b72378e165db540a6a5f15f720b40426
asyraff <145573395+ditt12@users.noreply.github.com>
2025-08-15 02:14:55 +0700

    Update tes.lua

--- tes.lua
+++ tes.lua
@@ -1,18 +1,1 @@
-local HttpService = game:GetService("HttpService")
 
-local webhookURL = "https://discord.com/api/webhooks/1381242557459075212/KNYM0-8i6pQgQyqbYFcY318HrKKuhuWYwlBS4gUTTEukozLJPL2adEhBM86QRJwJX2G9"  -- Ganti dengan URL webhookmu!
-
-local data = {
-    ["content"] = "Ada player baru nih!",
-    ["embeds"] = {{
-        ["title"] = "Info Player",
-        ["description"] = "Nama: " .. player.Name,
-        ["color"] = 65280  -- Warna hijau
-    }}
-}
-
-local headers = {
-    ["Content-Type"] = "application/json"
-}
-
-HttpService:PostAsync(webhookURL, HttpService:JSONEncode(data), Enum.HttpContentType.ApplicationJson, false, headers)
