1915e39f514c987c07b542ab1aeb09001ab9e4be
asyraff <145573395+ditt12@users.noreply.github.com>
2026-05-30 11:59:55 +0700

    Update fake.lua

--- fake.lua
+++ fake.lua
@@ -1,11 +1,11 @@
 local character = script.Parent
-
 local head = character:WaitForChild("Head")
 
 head.Transparency = 1
+head.CanCollide = false
 
-for _, obj in ipairs(head:GetDescendants()) do
-	if obj:IsA("Decal") then
-		obj.Transparency = 1
+for _, v in ipairs(head:GetChildren()) do
+	if v:IsA("Decal") then
+		v.Transparency = 1
 	end
 end
